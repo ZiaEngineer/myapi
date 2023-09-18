df58496c659be4770c5066449476d99ddc014dfe
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-09-18 14:50:18 +0500

    my api

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
-const port = 3001;
+const port = process.env.PORT  || 3001;
+
+
+app.use(cors());
+
+const apiData = require('./data.json')
+
+app.get('/service',(req, res)=>{
+    res.send(apiData)
+})
+
 app.get("/", (req, res)=>{
     res.send('Hello i am live')
 });
