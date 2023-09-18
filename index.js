1d99681123b1609a83f77fe17a713567ad9a09fb
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-09-18 12:50:17 +0500

    node js project first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+app.get("/", (req, res)=>{
+    res.send('Hello i am live')
+});
+
+
+app.listen(port, ()=>{
+    console.log('heloo again live')
+})
